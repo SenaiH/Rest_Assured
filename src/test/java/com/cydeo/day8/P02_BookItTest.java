c50bdeb1eb50fc92954836a156ab9c4c1848f551
Senai <simoseni@gmail.com>
2023-08-23 10:25:22 -0700

    Day8 updated

---
+++ src/test/java/com/cydeo/day8/P02_BookItTest.java
@@ -0,0 +1,11 @@
+package com.cydeo.day8;
+
+import com.cydeo.utilities.BookItTestBase;
+
+public class P02_BookItTest extends BookItTestBase {
+
+    String accessToken = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIxODMxNiIsImF1ZCI6InN0dWRlbnQtdGVhbS1sZWFkZXIifQ.AfWgzpG2pL-Sdq099hoRVsADQwTTrj6tCtYddW1wIRM";
+
+
+
+}
